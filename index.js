013b5f214de94fb03bfa1ff34e670affd41f85cc
meow <you@example.com>
2021-07-06 06:41:39 -0700

    first_and_last_name_Var_let

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
   console.log("hello world");
   let name= 'David';
   console.log(name);
+
+
+  //cannot be reserved keywords
+  //should be meaningful variable names
+  // cannont start with a number
+  // cannot containt a space or hypen
+  // case sensitive
+  let firstName= 'David'
+  let lastName= 'pegueros';
+
+console.log(firstName,lastName);
+
