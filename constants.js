acb828865eb1cd8788f8265e3754e99dfc5e43f5
meow <you@example.com>
2021-07-06 06:52:50 -0700

    primitive_types

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const interestRate= 0.3;
-interestRate=1;
+//interestRate=1;
 console.log(interestRate);
 
 //error should occur
---
+++ primitivetypes.js
@@ -0,0 +1,8 @@
+//strings, number,
+
+let name=" david"; //string literal
+let age =30; // number literal
+let isAprroved= true; // boolean literal 
+let firstName= undefined;
+let selectedColor= null; //will clear value from var
+// es6 has one more primitive symbol learned later 
